f6104dbd3ffa6a87fc506ff7a24fd70f4af82c5b
Xerin1 <tanatos683@gmail.com>
2026-03-05 12:34:51 +0100

    4. heti feladatok

--- App.java
+++ App.java
@@ -1,5 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Random rand = new Random();
+        int szam = rand.nextInt(100) + 1; // 1-100 közötti véletlen szám
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Találgass egy számot 1 és 100 között!");
+
+        while (true) {
+            System.out.print("Tipp: ");
+            int tipp = scanner.nextInt();
+
+            if (tipp < szam) {
+                System.out.println("Nagyobb!");
+            } else if (tipp > szam) {
+                System.out.println("Kisebb!");
+            } else {
+                System.out.println("Eltaláltad! A szám: " + szam);
+                break;
+            }
+        }
+
+        scanner.close();
     }
 }
